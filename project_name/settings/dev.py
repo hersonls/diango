df9ce8dc2d81f6d4c4e23b74dda20a946e5ee872
Hersonls <hersonls@gmail.com>
2013-04-09 17:52:23 -0300

    Im alive

---
+++ project_name/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+
+# Debug
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
